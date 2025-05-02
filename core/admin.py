54f97c691a5f3ae53f00ae378797b775219d60c5
Tommyzzz <Santan.web@gmail.com>
2025-05-02 22:19:54 +0000

    Register core models in admin

--- core/admin.py
+++ core/admin.py
@@ -1,15 +1,28 @@
 from django.contrib import admin
-from .models import User, VerificationStatus, Department, Function, Role, Position, Category, Product, Purchase, Reward, Ship, CorporationCheckSettings
+from .models import (
+    Category,
+    CorporationCheckSettings,
+    Department,
+    Function,
+    User,
+    VerificationStatus,
+    Ship,
+    Role,
+    Reward,
+    Purchase,
+    Product,
+    Position,
+)
 
-admin.site.register(User)
-admin.site.register(VerificationStatus)
+admin.site.register(Category)
+admin.site.register(CorporationCheckSettings)
 admin.site.register(Department)
 admin.site.register(Function)
-admin.site.register(Role)
-admin.site.register(Position)
-admin.site.register(Category)
-admin.site.register(Product)
-admin.site.register(Purchase)
-admin.site.register(Reward)
+admin.site.register(User)
+admin.site.register(VerificationStatus)
 admin.site.register(Ship)
-admin.site.register(CorporationCheckSettings)
+admin.site.register(Role)
+admin.site.register(Reward)
+admin.site.register(Purchase)
+admin.site.register(Product)
+admin.site.register(Position)
